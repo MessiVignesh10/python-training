daca9fbccc6e1438b54080edf5559e2d476f866e
MessiVignesh10 <110535676+MessiVignesh10@users.noreply.github.com>
2022-09-27 16:16:42 +0530

    Add files via upload

---
+++ Trained sample.py
@@ -0,0 +1,10 @@
+n=int(input("Enter any number"))
+if (n%2==0):
+    if ( n>2 and n<5):
+        print("Not Weired")
+    elif (n > 6 and n<20):
+        print("Weired")
+    elif (n>20):
+        print("Not weired")
+else:
+    print("Weired")
---
+++ format.py
@@ -0,0 +1,2 @@
+x=49
+print("The total no of Rounds in F1 race is {}".format(x))
