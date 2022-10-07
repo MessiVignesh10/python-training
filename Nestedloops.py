5d72de1c30a8ba440e929c10afe90c3e568d122e
MessiVignesh10 <110535676+MessiVignesh10@users.noreply.github.com>
2022-10-07 16:27:38 +0530

    Add files via upload

---
+++ Nestedloops.py
@@ -0,0 +1,6 @@
+numbers=[1,2,3]
+Weekdays=['MONDAY','TUESDAY','WEDNESDAY']
+for i in numbers:
+     print(i)
+     for j in Weekdays:
+          print(j)
---
+++ Nestedloops2.py
@@ -0,0 +1,5 @@
+for i in range(1,5):
+     for j in range(i):
+           print(start='    ',i,end=' ')
+     print()
+          
